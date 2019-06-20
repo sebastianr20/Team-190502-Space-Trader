72c8d128ffc6d545bc393d29aa43754a298e4f97
dsachdeva6 <47339875+dsachdeva6@users.noreply.github.com>
2019-06-19 21:22:10 -0400

    Create Difficulty.java

---
+++ 190502SpaceTraders/app/src/main/java/com/example/spacetraders190502/Model/Difficulty.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+public enum Difficulty {
+    BEGINNER("Beginnner"),
+    EASY("Easy"),
+    NORMAL("Normal"),
+    HARD("Hard"),
+    IMPOSSIBLE("Impossible");
+
+    private final String returnDifficulty;
+    Difficulty (String returnDifficulty){
+        this.returnDifficulty = returnDifficulty;
+    }
+
+    public String getReturnDifficulty() {
+        return returnDifficulty;
+    }
+}
